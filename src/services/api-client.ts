f598ec13f6fb9596de639e4dc11648edfb1b486a
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-14 13:25:42 +0300

    protecting API keys

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params:{
-        key: '0c854bf4da844b09a2b0a8a88ba80403'
+        key: import.meta.env.VITE_RAWG_API
     }
 })
