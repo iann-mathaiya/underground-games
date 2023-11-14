236cb02ef44991f48612555d978f268e97700ceb
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-14 13:42:55 +0300

    added an AbortController in the useGames hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import apiClient from "@/services/api-client"
+import { CanceledError } from "axios"
 
 interface Game {
   id: number
@@ -16,11 +17,18 @@ export default function useGames() {
   const [error, setError] = useState("")
 
   useEffect(() => {
+    const controller = new AbortController()
+
     apiClient
-      .get<FetchGamesResponse>("/games")
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((error) => setError(error.message))
+      .catch((error) => {
+        if (error instanceof CanceledError) return
+        setError(error.message)
+      })
+
+    return () => controller.abort()
   }, [])
-  
+
   return { games, error }
 }
