0c0a40431262f445ab79c44a49759d5793913adb
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-14 10:30:19 +0300

    building the root layout of the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-
+import Main from "./components/Main"
+import NavBar from "./components/NavBar"
+import Sidebar from "./components/Sidebar"
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <html className='h-full bg-gray-100 dark:bg-slate-800'>
+      <body className='h-full'>
+        <main>
+          <NavBar />
+          <Sidebar />
+          <Main />
+        </main>
+      </body>
+    </html>
   )
 }
 
