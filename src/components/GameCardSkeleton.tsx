ff4130ea59504e769ad64bd40fd2dde86e16944d
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-15 10:40:30 +0300

    implemented the game card skeleton to the grid

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,15 +1,15 @@
 export default function GameCardSkeleton() {
   return (
-    <div className='border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto'>
-      <div className='p-1 h-56 animate-pulse flex flex-col space-x-4 rounded-xl'>
-        <div className=" aspect-video rounded-lg" />
+    <div className='bg-slate-50 shadow rounded-md max-w-sm w-full'>
+      <div className='p-1 h-fit animate-pulse rounded-xl'>
+        <div className="aspect-video rounded-lg bg-slate-300" />
 
-        <div className="p-2 space-y-4">
-            <div className="p-2 w-3/4 rounded-full" />
+        <div className="mx-2 py-2 space-y-3">
+            <div className="p-1.5 w-3/4 rounded-full bg-slate-300" />
 
-            <div className="flec items-center justify-between">
-                <div className="p-2 w-2/3 rounded-full" />
-                <div className="p-2 w-1/6 rounded-full" />
+            <div className="flex items-center justify-between">
+                <div className="p-1.5 w-2/3 rounded-full bg-slate-300" />
+                <div className="p-1.5 w-1/12 rounded-sm bg-slate-300" />
             </div>
 
         </div>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,21 @@
 import { Fragment } from "react"
 import useGames from "@/hooks/useGames"
 import GameCard from "./GameCard"
+import GameCardSkeleton from "./GameCardSkeleton"
 
 export default function GameGrid() {
-  const { games, error } = useGames()
+  const { games, error, isLoading } = useGames()
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
   return (
     <Fragment>
       {error && <p className='text-red-500 text-base font-medium'>{error}</p>}
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+
+      <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
-         <GameCard key={game.id} game={game} />
+          <GameCard key={game.id} game={game} />
         ))}
       </ul>
     </Fragment>
