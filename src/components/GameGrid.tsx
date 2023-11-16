fec0fe767bb246a8498a0ff7f4aa2ae200c7ba16
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-16 15:20:05 +0300

    added a intuitive UI to show users incase a platform doesn't have any games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,6 +19,27 @@ export default function GameGrid({ gameQuery }: Props) {
       <ul className='list-none grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
         {isLoading &&
           skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+
+        {games.length === 0 && (
+          <div className=' col-span-4 flex flex-col items-center justify-center gap-4'>
+            <img
+              className='w-40 h-40 object-cover object-center'
+              src='/src/assets/old-school-game-console.webp'
+              alt='Old Game Console'
+            />
+            <p className='text-lg lg:text-base text-slate-500 font-normal'>
+              No games in this platform
+            </p>
+            <button
+              className='py-1.5 px-3 text-sm text-orange-500 border border-orange-500 bg-orange-50 hover:bg-orange-500 hover:text-white rounded-lg'
+              onClick={() => location.reload()}
+            >
+              {" "}
+              Reload Page
+            </button>
+          </div>
+        )}
+
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
