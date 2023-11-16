90b3cfd03d3c7409b841904e34d9d930540b4e25
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-16 09:32:04 +0300

    added more platform icons

--- src/components/PlatformIcons.ts
+++ src/components/PlatformIcons.ts
@@ -1,7 +1,7 @@
 import { IconType } from "react-icons"
 import { BsGlobe } from "react-icons/bs"
-import { SiNintendo } from "react-icons/si"
 import { MdPhoneIphone } from "react-icons/md"
+import { SiNintendo, SiAtari, SiSega, SiCommodore } from "react-icons/si"
 import {
   FaWindows,
   FaPlaystation,
@@ -11,14 +11,17 @@ import {
   FaAndroid,
 } from "react-icons/fa"
 
-export const Icons : { [key: string]: IconType } = {
-    xbox: FaXbox,
-    mac: FaApple,
-    web: BsGlobe,
-    pc: FaWindows,
-    linux: FaLinux,
-    ios: MdPhoneIphone,
-    android: FaAndroid,
-    nintendo: SiNintendo,
-    playstation: FaPlaystation,
+export const Icons: { [key: string]: IconType } = {
+  xbox: FaXbox,
+  mac: FaApple,
+  web: BsGlobe,
+  sega: SiSega,
+  pc: FaWindows,
+  linux: FaLinux,
+  atari: SiAtari,
+  ios: MdPhoneIphone,
+  android: FaAndroid,
+  nintendo: SiNintendo,
+  commodore: SiCommodore,
+  playstation: FaPlaystation,
 }
